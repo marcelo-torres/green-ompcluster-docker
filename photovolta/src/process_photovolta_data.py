349caff19c0043eba48ce865f6b837d6552b1cc1
Marcelo Torres <marcelo.torres.o@hotmail.com>
2023-11-24 20:06:02 -0300

    photovolta data update

--- photovolta/src/process_photovolta_data.py
+++ photovolta/src/process_photovolta_data.py
@@ -7,10 +7,13 @@ def to_date_time(date_time_str):
     return datetime.strptime(date_time_str, date_format)
 
 
-def process_photovolta_data(source_file, new_file):
+def process_photovolta_data(source_file, new_file, date_time_limit):
 
     INTERVAL_SIZE = 300 # 300s = 5min
     ERROR_TOLERANCE_IN_SECONDS = 2
+
+    def must_remove_data(date_time):
+        return date_time_limit is not None and date_time < date_time_limit
 
     with open(source_file, 'r', encoding='UTF-8') as photovolta_file:
         with open(new_file, 'w', encoding='UTF8') as photovolta_new_file:
@@ -26,6 +29,7 @@ def process_photovolta_data(source_file, new_file):
                 
                 row = line.strip().split(',')
                 date_time = to_date_time(row[0])
+
                 solar_irradiance = row[1]
                 
                 time_stamp = date_time.timestamp()
@@ -65,7 +69,10 @@ def process_photovolta_data(source_file, new_file):
                         offset+=INTERVAL_SIZE
                         #print(f'{omitted_date_time} {INTERVAL_SIZE} {omitted_solar_irradiance} ({i}) {offset}')
                         
-                        writer.writerow([omitted_date_time, offset, omitted_solar_irradiance])
+                        if not must_remove_data(omitted_date_time):
+                            writer.writerow([omitted_date_time, offset, omitted_solar_irradiance])
+                        else:
+                            print(f'ignoring {date_time}')
                         
                     offset+=INTERVAL_SIZE
                 else:
@@ -73,11 +80,20 @@ def process_photovolta_data(source_file, new_file):
                     print(f'Error: Diff={diff} cannot be handled')
                
                 #print(f'{date_time} {time_stamp_diff} {solar_irradiance} {offset}')
-                writer.writerow([date_time, offset, solar_irradiance])
-                
-                previous_time_stamp = time_stamp
+                if not must_remove_data(date_time):
+                    writer.writerow([date_time, offset, solar_irradiance])
+                else:
+                    print(f'ignoring {date_time}')
+
+                previous_time_stamp = time_stamp  
 
 if __name__ == '__main__':
-    process_photovolta_data('../data/photovolta_2016_raw_data_part_1.csv', '../data/photovolta_2016_part_1.csv')
-    process_photovolta_data('../data/photovolta_2016_raw_data_part_2.csv', '../data/photovolta_2016_part_2.csv')
+    process_photovolta_data('../data/photovolta_2016_raw_data_part_1.csv', '../data/photovolta_2016_part_1.csv', None)
+    
+    # The second interval begins at 2016-11-16 11:10:01, but it is not known if it was omitted because the solar 
+    # irradiation was zero or if the data collection started at this time. So, this data will be removed until 
+    # the next day.
+    date_time_limit = datetime(2016, 11, 17, 0, 0, 0)
+    process_photovolta_data('../data/photovolta_2016_raw_data_part_2.csv', '../data/photovolta_2016_part_2.csv', date_time_limit)
+
 
