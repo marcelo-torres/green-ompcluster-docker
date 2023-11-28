a5cc6b6f3410c84f199491d3a7a202571958b32b
Marcelo Torres <marcelo.torres.o@hotmail.com>
2023-11-27 21:51:26 -0300

    output formatting

--- experiment/run_experiments.sh
+++ experiment/run_experiments.sh
@@ -44,24 +44,36 @@ do
     echo ==== $experiment ====
     echo
 
+    echo >> $output_file
+    echo ==== $experiment ==== >> $output_file
+    echo >> $output_file
+
     for i in "${!algorithms[@]}"
     do
         algoritm="${algorithms[$i]}"
         export OMPCLUSTER_SCHEDULER=$algoritm
 
+        echo $algoritm
+        echo
+        echo $algoritm >> $output_file
+        echo >> $output_file
+
         for j in "${!topologies[@]}"
         do
             topology="${topologies[$j]}"
 
-            echo $algoritm: $topology
+            echo $topology
+            echo $topology >> $output_file
 
             experiment_path=$base_path/$topology
             export OMPCLUSTER_SCHEDULER_INPUT_DIRECTORY=$experiment_path
 
 
             output=$(mpirun -np 24 /task-bench/ompcluster/main -steps 20 -width 10 -type $topology -kernel compute_bound -iter 1 | grep -E 'Objectives|Makespan')
+            echo $output
             echo $output >> $output_file
             echo
+            echo >> $output_file
         done
     done
 
